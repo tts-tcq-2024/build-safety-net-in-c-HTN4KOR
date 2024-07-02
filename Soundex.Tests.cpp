ebe98914be1f34941319da8dd6f70866a9d9f083
HTN4KOR <167968062+HTN4KOR@users.noreply.github.com>
2024-07-02 09:51:15 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,8 +5,8 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
- generateSiundex("BHF",soundex);
- generateSiundex("BAF",soundex);
- generateSiundex("L321",soundex);
+ //generateSiundex("BHF",soundex);
+// generateSiundex("BAF",soundex);
+// generateSiundex("L321",soundex);
  // ASSERT_EQ(soundex,"A200");
 }
