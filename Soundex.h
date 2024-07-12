a7f771343c2632244e21ca5df12158e05694881b
HTN4KOR <167968062+HTN4KOR@users.noreply.github.com>
2024-07-12 09:56:18 +0530

    Update Soundex.h
    build

--- Soundex.h
+++ Soundex.h
@@ -1,11 +1,9 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
-
-#include "Soundex.h"
+#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
-/*char getSoundexCode(char c) {
+ 
+// Function to get the Soundex code for a given character
+char getSoundexCode(char c) {
     c = toupper(c);
     switch (c) {
         case 'B': case 'F': case 'P': case 'V': return '1';
@@ -14,56 +12,57 @@
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+        default: return '0';
     }
-}*/
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
-    char arr[128];
-    arr['B'] = '1';
-    arr['F'] = '1';
-    arr['P'] = '1';
-    arr['V'] = '1';
-    arr['C'] = '2';
-    arr['G'] = '2'; arr['J'] = '2'; arr['K'] = '2'; arr['Q'] = '2'; arr['S'] = '2'; arr['X'] = '2'; arr['Z'] = '2';
-    arr['D'] = '3'; arr['T'] = '3';
-    arr['L'] = '4';
-    arr['M'] = '5'; arr['N'] = '5';
-    arr['R'] = '6';
-    arr['A'] = '0'; arr['E'] = '0'; arr['I'] = '0'; arr['O'] = '0'; arr['U'] = '0'; arr['H'] = '0'; arr['W'] = '0'; arr['Y'] = '0';
-    
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char code = arr[toupper(name[i])];
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-        }
-    }
-    
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
-    }
-
-    soundex[4] = '\0';
 }
-/*void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
-
-    for (int i = 1; i < len && sIndex < 4; i++) {
+ 
+// Function to check if a code is valid
+int isValidCode(char code, char prevCode) {
+    return code != '0' && code != prevCode;
+}
+ 
+// Function to check if the Soundex length is valid
+int isSoundexLengthValid(const char* soundex) {
+    return strlen(soundex) < 4;
+}
+ 
+// Function to add a Soundex code
+void addSoundexCode(char* soundex, char code, char* prevCode) {
+    if (isValidCode(code, *prevCode) && isSoundexLengthValid(soundex)) {
+        int len = strlen(soundex);
+        soundex[len] = code;
+        soundex[len + 1] = '\0';
+        *prevCode = code;
+    }
+}
+ 
+// Function to update the Soundex code based on the name
+void updateSoundex(char* soundex, const char* name, char* prevCode) {
+    for (int i = 1; name[i] != '\0'; ++i) {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-        }
+        addSoundexCode(soundex, code, prevCode);
     }
-
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
+    while (strlen(soundex) < 4) {
+        strcat(soundex, "0");
     }
-
-    soundex[4] = '\0';
-}*/
-
-#endif // SOUNDEX_H
+}
+ 
+// Function to generate the Soundex code for a given name
+void generateSoundex(const char* name, char* soundex) {
+    if (name[0] == '\0') {
+        soundex[0] = '\0';
+        return;
+    }
+    soundex[0] = toupper(name[0]);
+    soundex[1] = '\0';
+    char prevCode = getSoundexCode(soundex[0]);
+    updateSoundex(soundex, name, &prevCode);
+}
+ 
+int main() {
+    const char* name = "Example";
+    char soundex[5] = "";  // Initialize the soundex string
+    generateSoundex(name, soundex);
+    printf("Soundex code for %s is %s\n", name, soundex);
+    return 0;
+}
